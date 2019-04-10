1254f6518039057382df45b33bf9ac8e4613d1f2
Derek Wood <derek@sheriffderek.consulting>
2019-04-10 11:50:23 -0700

    Remove ‘thing’ resource stuff +
    - I’ll add these later to test that I understand the steps

--- models/thing.js
+++
@@ -1,8 +0,0 @@
-export default (sequelize, DataTypes)=> {
-  const Thing = sequelize.define('Thing', { // automatically maps to the 'Thing' table (in the database)
-    first: DataTypes.STRING,
-    last: DataTypes.STRING
-  });
-
-  return Thing;
-}
