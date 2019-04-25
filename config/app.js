be28605edb1f1b8424eae86ca83a917ac9467bd5
Derek Wood <derek@sheriffderek.consulting>
2019-04-25 11:10:33 -0700

    notes

--- config/app.js
+++ config/app.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); // load env variables
+// https://github.com/motdotla/dotenv $todo - get a better understanding of this
+ 
+// loads .env constants - and makes them available from process?
 
 export default {
   environment: process.env.NODE_ENVIRONMENT,
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // must use commonjs format in this file
+// sets you up to use import & export / es2015 stuff instead
 
 require = require('esm')(module);
 require('./main');
